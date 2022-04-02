9bd577f8ad44e44f0212367ac05d460d1c82ef6c
meherujjahan <jikrarjahan441@gmail.com>
2022-04-02 22:40:43 +0600

    added

---
+++ .history/index_20220210124345.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const app = express();
+var cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.daplg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//console.log(uri);
+
+async function run() {
+  try {
+    await client.connect();
+   //console.log(client)
+
+
+  } finally {
+  
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
